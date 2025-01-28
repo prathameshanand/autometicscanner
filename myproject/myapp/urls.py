d4a7723674997f8f27167613833ecf83ccbb89f7
Prathamesh <140943397+prathameshanand@users.noreply.github.com>
2025-01-28 05:41:39 -0500

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views import signup, login_view, logout_view, home
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    
     path('home/', home, name='home')
     
 ]
